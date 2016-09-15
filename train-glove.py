d20e6fc962cfa08211ba931edbbe9991519a11aa
Morgan Giraud <morgan@explee.com>
2016-09-15 17:54:35 +0200

    Add load/save capacities

---
+++ train-glove.py
@@ -0,0 +1,15 @@
+import time
+import tf_glove
+
+with open('crawler/data/results.txt', 'r') as f:
+    corpus = f.readlines()
+    for i,line in enumerate(corpus):
+        corpus[i] = line.lower().strip().split(' ')
+
+print('Init the GloVe model')
+model = tf_glove.GloVeModel(embedding_size=200, context_size=10, learning_rate=1e-3)
+print('Fit to corpus and compute graph for training')
+model.fit_to_corpus(corpus)
+print('Start Training')
+model.train(num_epochs=500, log_dir='results/' + str(int(time.time())), summary_batch_interval=100, should_save=True)
+model.print_sum_up()
