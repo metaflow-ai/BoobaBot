c32f7c330b45124489032dbd245fcda9925c03ff
Morgan Giraud <morgan@explee.com>
2016-09-19 12:09:12 +0200

    Add random search on training

--- train-glove.py
+++ train-glove.py
@@ -1,4 +1,6 @@
 import time, os
+import numpy as np
+
 import tf_glove
 
 dir = os.path.dirname(os.path.realpath(__file__))
@@ -7,10 +9,15 @@ with open(dir + '/crawler/data/results.txt', 'r') as f:
     for i,line in enumerate(corpus):
         corpus[i] = line.lower().strip().split(' ')
 
-print('Init the GloVe model')
-model = tf_glove.GloVeModel(embedding_size=200, context_size=10, learning_rate=1e-3)
-print('Fit to corpus and compute graph for training')
-model.fit_to_corpus(corpus)
-print('Start Training')
-model.train(num_epochs=500, log_dir=dir + '/results/' + str(int(time.time())), summary_batch_interval=100, should_save=True)
-model.print_sum_up()
+for i in range(10):
+    # Random search
+    embedding_size = int(np.random.random_integers(50,300))
+    context_size = int(np.random.random_integers(3,10))
+
+    print('Init the GloVe model')
+    model = tf_glove.GloVeModel(embedding_size=embedding_size, context_size=context_size, learning_rate=1e-3)
+    print('Fit to corpus and compute graph for training')
+    model.fit_to_corpus(corpus)
+    print('Start Training')
+    model.train(num_epochs=600, log_dir=dir + '/results/' + str(int(time.time())), summary_batch_interval=100, should_save=True)
+    model.print_sum_up()
